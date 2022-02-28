2fdda205e9f80ad5f91503ec86b870c498afe5fc
jdvannest (macbook) <jordanvannest@gmail.com>
2022-02-27 18:50:35 -0600

    Added overwrite flag to Run.py

--- Run.py
+++ Run.py
@@ -3,6 +3,7 @@ import os,argparse,pickle
 parser = argparse.ArgumentParser(description='Run collection of burstiness profiles for all simulations, or a specific one')
 parser.add_argument("-n","--numproc",type=int,default=1,help='The number of processes to use (default=1)')
 parser.add_argument("-s","--simulation",choices=['cptmarvel','elektra','storm','rogue','h148','h229','h242','h329'])
+parser.add_argument("-o","--overwrite",action='store_true')
 args = parser.parse_args()
 
 try:
@@ -14,8 +15,10 @@ except:
     os.system('mv SimulationInfo.pickle Code/')
     config = pickle.load(open('Code/Config.pickle','rb'))
 
+o = '-o' if args.overwrite else ''
+
 if args.simulation in ['cptmarvel','elektra','storm','rogue','h148','h229','h242','h329']:
-    os.system(f'{config["python_path"]} Code/BurstinessCalculation.py -s {args.simulation} -n {args.numproc}')
+    os.system(f'{config["python_path"]} Code/BurstinessCalculation.py -s {args.simulation} -n {args.numproc} {o}')
 else:
     for sim in ['cptmarvel','elektra','storm','rogue']:#,'h148','h229','h242','h329']:
-        os.system(f'{config["python_path"]} Code/BurstinessCalculation.py -s {sim} -n {args.numproc}')
+        os.system(f'{config["python_path"]} Code/BurstinessCalculation.py -s {sim} -n {args.numproc} {o}')
