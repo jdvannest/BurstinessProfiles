0a08fd566c668d9c0c78b8e1629aeeacae7ce249
jdvannest (macbook) <jordanvannest@gmail.com>
2022-02-28 10:46:18 -0600

    Fixed error where -o in Run.py overwrote after every sim-run

--- Run.py
+++ Run.py
@@ -15,10 +15,10 @@ except:
     os.system('mv SimulationInfo.pickle Code/')
     config = pickle.load(open('Code/Config.pickle','rb'))
 
-o = '-o' if args.overwrite else ''
-
 if args.simulation in ['cptmarvel','elektra','storm','rogue','h148','h229','h242','h329']:
+    o = '-o' if args.overwrite else ''
     os.system(f'{config["python_path"]} Code/BurstinessCalculation.py -s {args.simulation} -n {args.numproc} {o}')
 else:
     for sim in ['cptmarvel','elektra','storm','rogue']:#,'h148','h229','h242','h329']:
+        o = '-o' if (args.overwrite and sim=='cptmarvel') else ''
         os.system(f'{config["python_path"]} Code/BurstinessCalculation.py -s {sim} -n {args.numproc} {o}')
